3e8ce804ad9badc7392f04aa857eb6696f145686
Farhan Rahman <farhanrahman@Fuads-MacBook-Air.local>
2017-10-29 17:33:40 -0400

    Updating python shit

--- api/main.py
+++ api/main.py
@@ -1,17 +1,3 @@
-# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
 
 """Simple image classification with Inception.
 
@@ -142,6 +128,8 @@ def run_inference_on_image(image):
   """
   if not tf.gfile.Exists(image):
     tf.logging.fatal('File does not exist %s', image)
+
+
   image_data = tf.gfile.FastGFile(image, 'rb').read()
 
   # Creates graph from saved GraphDef.
@@ -194,7 +182,7 @@ def maybe_download_and_extract():
 
 
 def main(_):
-
+  maybe_download_and_extract()
   image_file = sys.argv[1];
   image = ('api/' + image_file)
   run_inference_on_image(image)
@@ -230,5 +218,6 @@ if __name__ == '__main__':
       default=5,
       help='Display this many predictions.'
   )
+
   FLAGS, unparsed = parser.parse_known_args()
   tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
